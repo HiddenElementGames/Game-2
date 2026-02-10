3e1402ee8a39427d2b7e6433b0baa12f923616f8
Keaton <kmiller3@uccs.edu>
2026-02-10 02:01:16 -0700

    Implemented full menu navigation to switch scenes and cameras

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public class GameCamera : MonoBehaviour
 {
+    [SerializeField] public SceneID sceneID;
+
     void Awake()
     {
-        gameObject.SetActive(false);
+        AddCameraToMenuNavigation();
+
+		// Deactivate all cameras except the home scene camera
+		if (sceneID != SceneID.Home)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+	/// <summary>
+	/// Adds this camera to the MenuNavigation system so it can be activated when the corresponding menu is selected
+	/// </summary>
+	private void AddCameraToMenuNavigation()
+    {
+        MenuNavigation menu = FindAnyObjectByType<MenuNavigation>();
+        menu.SceneCameras.Add(sceneID, gameObject);
 	}
 }
