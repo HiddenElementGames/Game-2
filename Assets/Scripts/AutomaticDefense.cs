ba712f52519088cce5279a4df5a77db00bc52c80
Keaton <kmiller3@uccs.edu>
2026-02-17 00:17:52 -0700

    Automatic defense now costs ammo or defense items

--- Assets/Scripts/AutomaticDefense.cs
+++ Assets/Scripts/AutomaticDefense.cs
@@ -1,7 +1,19 @@
+using System.Collections;
 using UnityEngine;
 
 public class AutomaticDefense : MonoBehaviour
 {
+	[SerializeField, Min(0)] private int minAmmoCost;
+	[SerializeField, Min(0)] private int maxAmmoCost;
+	[SerializeField, Min(0)] private float zombieKillDelay;
+
+	private WaitForSeconds ZombieKillDelay;
+
+	private void Start()
+	{
+		ZombieKillDelay = new WaitForSeconds(zombieKillDelay);
+	}
+
 	private void OnTriggerEnter(Collider other)
 	{
 		if(other.CompareTag("Zombie"))
@@ -9,13 +21,28 @@ public class AutomaticDefense : MonoBehaviour
 			// only try to defend the base if there are available defenses, or if there are survivors and ammo
 			if (GameManager.Instance.SurvivorCount > 0 && GameManager.Instance.AmmoCount > 0)
 			{
-				Destroy(other.gameObject);
+				StartCoroutine(AttackZombieDelayed(other.gameObject));
 			}
 			else if(GameManager.Instance.DefenseItemCount > 0)
 			{
+				GameManager.Instance.DefenseItemCount--;
 				Destroy(other.gameObject);
 			}
 			// keeping the above 2 situations separate, that way we can properly subtract defense count or ammo count when the time comes
 		}
+	}
+
+	private IEnumerator AttackZombieDelayed(GameObject zombie)
+	{
+		// wait
+		yield return ZombieKillDelay;
+
+		// consume ammo
+		int ammoCost = Random.Range(minAmmoCost, maxAmmoCost + 1);
+		ammoCost = Mathf.Min(ammoCost, GameManager.Instance.AmmoCount);
+		GameManager.Instance.AmmoCount -= ammoCost;
+
+		// kill zombie
+		Destroy(zombie);
 	}
 }
